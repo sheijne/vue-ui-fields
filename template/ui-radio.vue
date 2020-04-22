1249c737fd8978fab73b0b003c126821b0c91635
Guus Dijkhuis <guus.dijkhuis@gmail.com>
2020-04-22 11:34:17 +0200

    fix radiobutton selected

--- template/ui-radio.vue
+++ template/ui-radio.vue
@@ -7,12 +7,12 @@
 			<input
 				:id="`${form}_${name}_${key}`"
 				:key="key"
-				v-model="value"
 				:value="option.value"
 				:name="fieldData.name"
 				:type="fieldData.type"
 				v-bind="fieldData.htmlSettings"
 				:class="`${className}__input ${fieldData.type}__input`"
+				:checked="option.selected ? 'checked' : ''"
 				@blur="setValue"
 			/>
 			<label :key="`label_${key}`" :class="`${className}__element ${fieldData.type}__element`" :for="`${form}_${name}_${key}`">
