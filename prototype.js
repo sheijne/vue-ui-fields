68ff29e97c02feea4913d93e937ffd0dd154d777
Dennis Dipsaus <dennis.spierenburg@hotmail.com>
2020-05-05 14:36:06 +0200

    Dacht dat dit al gefixt was

--- prototype.js
+++ prototype.js
@@ -719,7 +719,7 @@ export default function(options, Vue) {
 				}
 			},
 			async new(formID) {
-				const URL = `${options.baseURL}/wp-json/matise/utilities/gfapi/${formID}?path=${window.location.pathname}`;
+				let URL = `${options.baseURL}/wp-json/matise/utilities/gfapi/${formID}?path=${window.location.pathname}`;
 				if (window.location.search.substr(1)) {
 					URL += `&${window.location.search.substr(1)}`;
 				}
